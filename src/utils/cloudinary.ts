1e48a4166a054ca94498db535f909a1867812218
Ennygenius <inuenike@gmail.com>
2024-04-01 02:45:40 -0700

    Finished touching Image Upload with cloudinary

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -5,11 +5,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.API_KEY,
-//   api_secret: process.env.API_SECRET,
-// });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
